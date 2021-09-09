16df06702d150db6af615165f6c4c1ba8c83e19a
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2021-09-09 15:19:39 +0200

    v1.0 Morsen

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(".")
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(1000)
+    radio.sendString("")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("-")
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(1000)
+    radio.sendString("")
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
